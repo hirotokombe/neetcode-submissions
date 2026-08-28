bb9fe23e1422f85759e2f5f874be07ef86a65517
Hiroto <96966143+hirotokombe@users.noreply.github.com>
2026-08-28 15:57:23 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        lst = [0] * (n + 1)
+        lst[0], lst[1] = 1, 1
+        
+        i = 2
+        while i < n + 1:
+            lst[i] = lst[i - 1] + lst[i - 2]
+            i += 1
+        return lst[n]
