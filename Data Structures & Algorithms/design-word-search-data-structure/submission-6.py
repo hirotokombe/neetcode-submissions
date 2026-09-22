19fe650783ab69e082cca43ba883ea79b3f56002
Hiroto <96966143+hirotokombe@users.noreply.github.com>
2026-09-22 17:42:22 -0400

    Add: design-word-search-data-structure - submission-6

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-6.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            curr = root
+            for i in range(j, len(word)):
+                if word[i] == '.':
+                    for childTrieNode in curr.children.values():
+                        if dfs(i + 1, childTrieNode):
+                            return True
+                    return False
+                else:
+                    if word[i] not in curr.children:
+                        return False
+                    curr = curr.children[word[i]]
+            return curr.word
+        return dfs(0, self.root)
+    
+            
